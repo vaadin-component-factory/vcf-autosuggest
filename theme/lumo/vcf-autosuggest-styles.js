f7d65ffeb8e3cb67cc88d826cdb30024d03f0163
Unknown <unknown@example.com>
2021-06-14 00:55:20 +0100

    1.0.4

--- theme/lumo/vcf-autosuggest-styles.js
+++ theme/lumo/vcf-autosuggest-styles.js
@@ -14,20 +14,6 @@ const overlayTheme = document.createElement('dom-module');
 overlayTheme.id = 'vcf-autosuggest-overlay-lumo';
 overlayTheme.setAttribute('theme-for', 'vcf-autosuggest-overlay');
 overlayTheme.innerHTML = `
-    <style>
-      :host [part='options-container'] {
-          margin-left: 0.5em;
-          margin-right: 0.5em;
-          min-width: var(--vcf-autosuggest-options-width);
-      }
-      :host [part="options-container"] vaadin-item::before {
-        content: none;
-      }
-      :host [part="dropdown-end-slot"] {
-        border-top: 1px solid var(--lumo-contrast-40pct);
-      }
-    </style>
-
     <template>
       <style>
         :host [part='options-container'] {
